77c1757bbeae3ec5b3c5d87539f425060944dcde
ranaPhero <ranaahmed3857516@gmail.com>
2022-08-18 13:30:59 +0600

    null vs undefined

---
+++ undefine-vs-null.js
@@ -0,0 +1,26 @@
+
+
+function add (num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2)
+}
+
+
+
+ const premik = {name: "smart dude", phone: 23456};
+ console.log(premik.gf);
+
+
+
+let fun = undefined;
+console.log(fun);
+
+
+null
+
+
